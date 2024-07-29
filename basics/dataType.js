c0edc689603e8901307ea9ecfc1faf9e84faf109
jayesh <jayeshsarvaiya9913@gamil.com>
2024-07-29 13:05:20 +0530

    add data type

---
+++ basics/dataType.js
@@ -0,0 +1,33 @@
+// console.log("we are in the data type");
+
+// alert("we are in the ") we are using the node it dont support the alert
+
+let person = [{ fname: "jayesh" }, { fname: "savan" }, { fname: "viral" }];
+
+console.log(person.length);
+
+// console.log(person[0].fname);
+// console.log(person[1].fname);
+// console.log(person[2].fname);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(null);
+
+// type conversion
+
+// let age = 21;
+// console.log(typeof age);
+// newAge = String(age);
+// console.log(typeof newAge);
+let srt = "123dfd";
+let numberStr = Number(srt);
+
+console.log(srt);
+console.log(numberStr);
+
+let isLogedin = 1;
+
+let booleanChange = Boolean(isLogedin);
+
+
+console.log(booleanChange);
--- basics/index.js
+++ basics/index.js
@@ -1,1 +1,32 @@
-console.log("we are in the basics and here we go again ");
+// console.log("we are in the basics and here we go again ");
+
+// let const var ki kahani
+
+// why you need the js
+
+// const name  = "javascript";
+// const age = 34
+// const address = "i dont know"
+// const mobile = 2323243523
+
+// console.log(name,age,address,mobile);
+
+// let accountId = 1223434;
+// const email = "email@gmail.com";
+// var accountPassword = 334534;
+
+// accountPassword = "we are in the password";
+// console.log(accountId, email, accountPassword);
+
+// let person = {
+//   fname: "jayesh",
+//   age: 34,
+//   address: "we dont know",
+// };
+
+// // console.log(person);
+
+// for (const key in person) {
+//   console.log(person[key]);
+// }
+
