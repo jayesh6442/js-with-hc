8fafe57a99a62492bf8901f1c6c776dc133eae7c
jayesh <jayeshsarvaiya9913@gamil.com>
2024-07-30 12:32:15 +0530

    add loops

--- loops/index.js
+++ loops/index.js
@@ -17,3 +17,67 @@
 // for (let i = 0; i < 10; i++) {
 //   console.log("jayesh");
 // }
+
+// for (let i = 0; i < 5; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     console.log("*");
+//   }
+// }
+// console.log("\n");
+
+// for (let i = 1; i < 101; i++) {
+//   for (let j = 1; j < 11; j++) {
+//     console.log(i +  " * " + j + "  = " + i * j);
+//   }
+// }
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// arr.forEach((value) => {
+//   console.log(typeof value);
+// });
+
+// const person = {
+//   name: "jayesh",
+//   age: 21,
+//   address: "gandhinagar",
+//   study: "b.tech",
+// };
+
+// for (const i in person) {
+//   console.log(i);
+// }
+
+// let person = {
+//   name: "jayesh",
+//   age: 21,
+//   address: "gandhinagar",
+//   study: "b.tech",
+// };
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (const i of arr) {
+//   console.log(arr[i]);
+// }
+
+// let name = "sarvaiya";
+
+// for (const iterator of name) {
+//   console.log(name[iterator]);
+// }
+
+// let person = {
+//   name: "jayesh",
+//   age: 21,
+//   address: "sarvaiya",
+// };
+
+// for (const [key] in person) {
+//   console.log();
+// }
+
+// let arr = ["a", "b", "c", "d", "e", "f"];
+// arr.forEach((element) => {
+//   console.log(element);
+// });
